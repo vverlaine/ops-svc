0f1acb5e955df0503ccd8366b87aeab8b1bc3d50
vvarlaine_apex <vvarlaine@apexglobal.co>
2025-10-17 15:10:43 -0600

    inital commit

---
+++ src/main/java/com/proyecto/ops_svc/OpsSvcApplication.java
@@ -0,0 +1,13 @@
+package com.proyecto.ops_svc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpsSvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpsSvcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/proyecto/ops_svc/OpsSvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyecto.ops_svc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpsSvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
